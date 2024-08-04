9d9213d091ae6647859635f1442f7598bae8cc73
VitorLeiteJr <96vitorleite@gmail.com>
2024-08-03 22:06:50 -0300

    config: primsa db config

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,32 @@
+import Button from "@/components/ui/Button";
+import Link from "next/link";
+
 const Home = () => {
   return ( 
-  <div 
-  className="bg-[url('/bg.jpeg')] h-[102vh] 
-  relative w-full bg-cover mt-[-75px] overflow-hidden
-  text-white"> 
-  <div className="flex h-full items-center justify-center pt-[82px]
-  gap20 w-[90%] mx-auto max-w-[1450px]">
-    <div className="grid items-center gap-6 md:grind-cols-2">
-      <img src="/hero-image.png"alt="product img"
-            className="ax-auto rounded-xl order-last md:min-2-[800px]
-            mn-2-[500w] md:h-[500px] max-sm:px-5"></img>
+    <div className="bg-[url('/bg.jpeg')] h-[102vh] relative w-full bg-cover mt-[-75px] overflow-hidden text-white">
+    <div className="flex h-full items-center justify-center pt-[82px] gap20 w-[90%] mx-auto max-w-[1450px]">
+      <div className="grid items-center gap-6 md:grid-cols-2">
+        <img
+          alt="product image"
+          className="mx-auto rounded-xl order-last md:min-w-[800px] min-w-[500px] md:h-[500px] max-sm:px-5"
+          src="/hero-image.png"
+        />
+        <div className="flex flex-col justify-center max-md:items-center space-y-4 max-md:text-center">
+          <div className="space-y-2">
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl">
+              Visualize Success Daily
+            </h2>
+            <p className="max-w-[500px] md:text-xl text-gray-300">
+              Take control of your projects with our
+              simple yet powerful Kanban board.
+            </p>
+          </div>
+          <Link href={"/sign-up"}>
+            <Button text="Start planning now &#8594;" />
+          </Link>
+        </div>
+      </div>
     </div>
-  </div>
   </div> );
 }
  
